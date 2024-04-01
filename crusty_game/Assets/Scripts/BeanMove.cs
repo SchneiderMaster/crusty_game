b29b9a92c305016b9c00e8e2b85df7c8ef5aa88e
SchneiderMaster <fynn2746@outlook.de>
2024-04-01 15:59:25 +0200

    do a barrel roll

---
+++ crusty_game/Assets/Scripts/BeanMove.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeanMove : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(0, 0, speed * 0.01f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(0, 0, -speed * 0.01f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-speed * 0.01f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(speed * 0.01f, 0, 0);
+        }
+
+        if (Cursor.lockState.Equals(CursorLockMode.Locked))
+        {
+            this.transform.Rotate(0 , Input.GetAxisRaw("Mouse X"), 0);
+            cam.transform.RotateAround(this.transform.position, Input.GetAxisRaw("Mouse Y"));
+        }
+
+
+    }
+}
